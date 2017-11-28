18eb1cb87f320fa96ae7b6adf3dd4acb549d6f9c
susanctg <susanctg@gmail.com>
2017-11-29 00:15:00 +0700

    set size

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.TeleportGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TeleportGame(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 600;
+        config.height = 900;
+        new LwjglApplication(new TeleportGame(), config);
+    }
 }
