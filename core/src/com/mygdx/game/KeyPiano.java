710f63f781ca207ccf937c9776c8393e9e05ab2a
susanctg <susanctg@gmail.com>
2017-12-17 21:00:15 +0700

    add randx

--- core/src/com/mygdx/game/KeyPiano.java
+++ core/src/com/mygdx/game/KeyPiano.java
@@ -2,9 +2,11 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.Random;
+
 public class KeyPiano{
     private Vector2 position;
-
+    Random random = new Random();
     public KeyPiano(int x, int y) {
         position = new Vector2(x,y);
     }
@@ -16,7 +18,9 @@ public class KeyPiano{
     public void move(){
         position.y += 5;
         if(position.y >= 1000){
-            //position.y = -250;
+            position.y = -250;
+            int randX = random.nextInt(3);
+            position.x = randX*200;
         }
     }
 }
